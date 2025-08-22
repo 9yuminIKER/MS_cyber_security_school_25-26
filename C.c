85f92140359ff631fc01dc313835e89a51a27411
unknown <etmg011104102@gmail.com>
2025-08-22 16:52:59 +0900

    ok

--- C.c
+++ C.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 int main(void)
+/*
 {
 
 	int age,score;
+
 	printf("나이가 어떻게 되세요?:"); scanf("%d",&age);
 	printf("점수가 어떻게 되세요?:"); scanf("%d",&score);
 		
 	printf("\n\nMy age:%d\n", age);
 	printf("%d is my point\n",score);
 	printf("Good\nmorning\neverybody");
-
+	
+	return 0;
 	//printf("My age:%d \n%d is my point\nGood\nmorning\neverybody"
- } 
+ }
+ */
  
+ {
+ 	
+ }
+ 
+ 
+ 
+
+ 
+	 
