52bd4df0af3c6f528a820a8e88ebe5693e133c16
unknown <etmg011104102@gmail.com>
2025-08-25 16:52:59 +0900

    ok

---
+++ C언어/ch3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int sval, eval, rval;
+	printf("시작값을 입력하세요:");scanf("%d",&sval);
+	//printf("종료값을 입력하세요:");scanf("%d",&eval);
+	
+	rval=0;
+	int index=sval;
+	while((index>=sval) && (index<=eval))
+	{
+		
+	}
+	rval=rval+sval; sval++;
+	rval=rval+sval; sval++;
+	rval=rval+sval; sval++;
+	rval=rval+sval; sval++;
+	rval=rval+sval; sval++;
+	rval=rval+sval; sval++;
+	rval=rval+sval; sval++;
+	rval=rval+sval; sval++;
+	
+	
+}
