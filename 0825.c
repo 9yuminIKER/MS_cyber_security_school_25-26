c1bbe834e0192c885e1caa5155cd4d7fd232583c
unknown <etmg011104102@gmail.com>
2025-08-25 16:21:58 +0900

    ok

--- 0825.c
+++ 0825.c
@@ -1,15 +1,67 @@
 #include <stdio.h>
-
 int main(void)
 {
-	int a;
-	int b;
+/*	int score;
 	
-	a=10;
-	b=20;
+	printf("점수를 입력하세요: ");
+	scanf("%d",&score);
 	
-	printf("%d %d\n", a,b);
-//	printf("%d %d\n", c,d);
+	if(score>=90)
+	{
+		printf("A");	
+	}
+	else if(score>=80)
+	{
+		printf("B");	
+	}
+	else if(score>=70)
+	{
+		printf("C");	
+	}
+	else if(score>=60)
+	{
+		printf("D");	
+	}
+	else
+	{
+		printf("F");
+	}
+} */
+	int sum=0;
+	double av;
+	int i;
+	int score;
+	int g;
 	
-	return 0;
+	for(i=1;i<6;i++)
+	{
+		printf("과목%d를 입력하세요:", i);
+		scanf("%d",&score);
+		sum=score+sum;
+	}
+	av=sum/5;
+	printf("총점:%d, 평균:%2.f",sum,av );
+
+	if(av>=90)
+	{
+		printf(" 학점:A");
+	}
+	else if(av>=80)
+	{
+		printf(" 학점:B");
+	}
+	else if(av>=70)
+	{
+		printf(" 학점:C");
+	}
+	else if(av>=60)
+	{
+		printf(" 학점:D");
+	}
+	else
+	{
+		printf(" 학점:F");
+	}
+	
+ 
 }
